cf88752e8fd92971960dc336f2a1fe210f7cc471
vikkoooo <vikkolundberg@gmail.com>
2024-09-17 11:25:06 +0200

    fix naming convention

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,15 @@ export function Navbar(): ReactElement {
 	return (
 		<header className="navbar">
 			<div className="navbar-container">
-				<Link to="/" className="link-home">
-					<span className="material-symbols-outlined avatar">account_circle</span>
+				<Link to="/" className="navbar-link-home">
+					<span className="material-symbols-outlined navbar-avatar">account_circle</span>
 				</Link>
 				<nav className="navbar-links">
-					<Link to="/student" className="link-student-dashboard">Student Dashboard</Link>
-					<Link to="/teacher" className="link-teacher-dashboard">Teacher Dashboard</Link>
-					<Link to="/course/1" className="link-course-details">Course Details 1</Link>
+					<Link to="/student" className="navbar-link-student-dashboard">Student Dashboard</Link>
+					<Link to="/teacher" className="navbar-link-teacher-dashboard">Teacher Dashboard</Link>
+					<Link to="/course/1" className="navbar-link-course-details">Course Details 1</Link>
 				</nav>
-				<button type="button" onClick={() => (console.log("logout clicked"))} className="button-logout">Logout</button>
+				<button type="button" onClick={() => (console.log("logout clicked"))} className="navbar-button-logout">Logout</button>
 			</div>
 		</header>
 	);
