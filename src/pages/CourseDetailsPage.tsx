24ac02311032f517aed9bb3a8039b5b318ff0687
vikkoooo <vikkolundberg@gmail.com>
2024-09-19 12:43:54 +0200

    remove dead code, moved to other place

--- src/pages/CourseDetailsPage.tsx
+++ src/pages/CourseDetailsPage.tsx
@@ -1,12 +1,11 @@
 import { ReactElement } from "react";
-import { CourseDetailsCard } from "../components/CourseDetailsCard";
 
 export function CourseDetailsPage(): ReactElement {
 	return (
-		<div className="course-details-container">
-			<h1>Details Dashboard</h1>
-			<div className="course-details-cards">
-				<CourseDetailsCard />
+		<div className="course-details-container g-container">
+			<h1 className="g-page-header">Details Dashboard</h1>
+			<div>
+				Lorem ipsum
 			</div>
 		</div>
 	);
