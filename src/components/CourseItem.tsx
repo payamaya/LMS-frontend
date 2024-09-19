2a45116fdde320c24267ecca2e51de8e57fbf354
OskarMartens <martens.oskar@gmail.com>
2024-09-19 11:57:32 +0200

    Rename CourseCard to Item

---
+++ src/components/CourseItem.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from "react";
+import { BasicCourseInfo } from "../interfaces";
+
+interface ICourseItem {
+  data: BasicCourseInfo;
+}
+
+export default function CourseItem({ data }: ICourseItem): ReactElement {
+  return (
+    <li className="g-list-item g-border-style">
+        <h3 className="g-list-item-header">{data.name}</h3>
+        <div className="g-list-item-text">
+          {data.startdate} - {data.startdate}
+        </div>
+      <div className="g-list-item-text">Teacher: {data.primary_teacher}</div>
+    </li>
+  );
+}
