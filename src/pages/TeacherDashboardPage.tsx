54effe9df6c0e3f0c1701419ed0e7aedb3e3b038
vikkoooo <vikkolundberg@gmail.com>
2024-09-19 14:20:51 +0200

    change course name

--- src/pages/TeacherDashboardPage.tsx
+++ src/pages/TeacherDashboardPage.tsx
@@ -4,28 +4,26 @@ import { CourseDetailsCard } from "../components/CourseDetailsCard";
 import CourseItem from "../components/CourseItem";
 
 export function TeacherDashboardPage(): ReactElement {
-  
-  const data: BasicCourseInfo = {
-    id: 1,
-    name: "C# LTU",
-    primary_teacher: "Linus Torvalds",
-    startdate: "2024-05-17",
-  };
 
-  return (
-    <div className="teacher-dashboard-container g-container">
-      <h1 className="g-page-header">Teacher Dashboard</h1>
-      <div className="g-page-card-container">
-        <div className="g-card">
-          <h2 className="g-card-header">Courses</h2>
-          <ul className="g-list">
-            <CourseItem data={data} />
-          </ul>
-        </div>
-        <div className="g-card">
-          <CourseDetailsCard />
-        </div>
-      </div>
-    </div>
-  );
+	const data: BasicCourseInfo = {
+		id: 1,
+		name: "Introduction to React",
+		primary_teacher: "Linus Torvalds",
+		startdate: "2024-05-17",
+	};
+
+	return (
+		<div className="teacher-dashboard-container g-container">
+			<h1 className="g-page-header">Teacher Dashboard</h1>
+			<div className="g-page-card-container">
+				<div className="g-card">
+					<h2 className="g-card-header">Courses</h2>
+					<ul className="g-list">
+						<CourseItem data={data} />
+					</ul>
+				</div>
+				<CourseDetailsCard />
+			</div>
+		</div >
+	);
 }
