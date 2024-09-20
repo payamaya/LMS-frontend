4d2426060cdc534c0c01898b95712b4ba2c30eaa
vikkoooo <vikkolundberg@gmail.com>
2024-09-20 11:30:11 +0200

    import mock data and use context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,26 @@
-import { ReactElement } from "react"
-import { Outlet, useLocation } from "react-router-dom"
-import { Navbar } from "./Navbar"
+import { ReactElement, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { Navbar } from "./Navbar";
+import { MockContext, MockData } from "../interfaces.ts";
+import mockData from "../courses-user-userid-mock.json";
 
 export function App(): ReactElement {
-
 	const location = useLocation();
+	const [data, setData] = useState<MockData>(mockData);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	// context
+	const mockContext: MockContext = {
+		data,
+		isLoading
+		//future API call functions here to pass down
+	};
 
 	return (
 		<div className="app">
 			{location.pathname !== '/' && <Navbar />}
 			<main className="main-content">
-				<Outlet context={null} />
+				<Outlet context={mockContext} />
 			</main>
 		</div >
 	)
