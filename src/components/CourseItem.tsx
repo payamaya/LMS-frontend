29aa154f3196eb3e97786000738a279821b8dfd5
OskarMartens <martens.oskar@gmail.com>
2024-09-20 09:36:18 +0200

    CourseItem is now selectable

--- src/components/CourseItem.tsx
+++ src/components/CourseItem.tsx
@@ -2,17 +2,19 @@ import { ReactElement } from "react";
 import { BasicCourseInfo } from "../interfaces";
 
 interface ICourseItem {
-	data: BasicCourseInfo;
+    data: BasicCourseInfo;
+    toggleActiveCourse: (id: number) => void;
+    isActive: boolean;
 }
 
-export default function CourseItem({ data }: ICourseItem): ReactElement {
-	return (
-		<li className="g-list-item g-border-style">
-			<h3 className="g-list-item-header">{data.name}</h3>
-			<div className="g-list-item-text">
-				{data.startdate} - {data.startdate}
-			</div>
-			<div className="g-list-item-text">Teacher: {data.primary_teacher}</div>
-		</li>
-	);
+export default function CourseItem({ data, toggleActiveCourse, isActive }: ICourseItem): ReactElement {
+    return (
+        <li className={`g-list-item g-border-style ${isActive ? "g-active" : ""}`} onClick={() => toggleActiveCourse(data.id)}>
+            <h3 className="g-list-item-header">{data.name}</h3>
+            <div className="g-list-item-text">
+                {data.startdate} - {data.startdate}
+            </div>
+            <div className="g-list-item-text">Teacher: {data.primary_teacher}</div>
+        </li>
+    );
 }
--- src/pages/TeacherDashboardPage.tsx
+++ src/pages/TeacherDashboardPage.tsx
@@ -1,29 +1,87 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { BasicCourseInfo } from "../interfaces";
 import { CourseDetailsCard } from "../components/CourseDetailsCard";
 import CourseItem from "../components/CourseItem";
 
 export function TeacherDashboardPage(): ReactElement {
+    const data: BasicCourseInfo[] =
+	[
+        {
+            id: 1,
+            name: "Introduction to React",
+            primary_teacher: "Linus Torvalds",
+            startdate: "2024-05-17",
+			students:
+			[
+				{
+					id: 3,
+					firstname: "Jonathan",
+					lastname: "Ek",
+					email: "eken@mail.com"
+				},
+				{
+					id: 4,
+					firstname: "Malin",
+					lastname: "Lindborg",
+					email: "malin.lindborg@mail.com"
+				}
+			],
+			modules:
+			[
+				{
+					id: 5,
+					module_name: "React Basics",
+					activites:
+					[
+						{
+							id: 6,
+							activity_name: "React Fundamentals Video",
+							type: "E-learning",
+							length: "1 hour"
+						},
+						{
+							id: 6,
+							activity_name: "React Fundamentals Video",
+							type: "Interactive",
+							length: "1 hour"
+						}
+					]
+				}
+			]
+        },
+        {
+            id: 2,
+            name: "Introduction to c#",
+            primary_teacher: "Linus Torvalds",
+            startdate: "2024-05-17",
+        },
+    ];
 
-	const data: BasicCourseInfo = {
-		id: 1,
-		name: "Introduction to React",
-		primary_teacher: "Linus Torvalds",
-		startdate: "2024-05-17",
-	};
+    const [activeCourse, setActiveCourse] = useState<BasicCourseInfo>(data[0]);
 
-	return (
-		<div className="teacher-dashboard-container g-container">
-			<h1 className="g-page-header">Teacher Dashboard</h1>
-			<div className="g-page-card-container">
-				<div className="g-card">
-					<h2 className="g-card-header">Courses</h2>
-					<ul className="g-list">
-						<CourseItem data={data} />
-					</ul>
-				</div>
-				<CourseDetailsCard />
-			</div>
-		</div >
-	);
+    const toggleActiveCourse = (id: number): void => {
+        const selectedCourse = data.find((course) => course.id === id);
+        if (selectedCourse) setActiveCourse(selectedCourse);
+    };
+
+    return (
+        <div className="teacher-dashboard-container g-container">
+            <h1 className="g-page-header">Teacher Dashboard</h1>
+            <div className="g-page-card-container">
+                <div className="g-card">
+                    <h2 className="g-card-header">Courses</h2>
+                    <ul className="g-list">
+                        {data.map((item) => (
+                            <CourseItem
+								key={item.id}
+								data={item}
+								toggleActiveCourse={toggleActiveCourse}
+								isActive={activeCourse.id === item.id} />
+                        ))}
+                    </ul>
+                </div>
+                <CourseDetailsCard />
+            </div>
+        </div>
+    );
 }
