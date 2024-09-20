828f03418820a66fffad56d47224d79cc9b6c361
OskarMartens <martens.oskar@gmail.com>
2024-09-20 14:59:24 +0200

    TeacherDashboard using dynamic values.

--- src/components/CourseDetailsCard.tsx
+++ src/components/CourseDetailsCard.tsx
@@ -1,13 +1,27 @@
 import { ReactElement } from "react";
 import { CourseDetailsModuleDropdown } from "./CourseDetailsModuleDropdown";
 import { CourseDetailsStudentsDropdown } from "./CourseDetailsStudentsDropdown";
+import { IDetailedCourse, IModule, IUser } from "../interfaces";
 
-export function CourseDetailsCard(): ReactElement {
+interface ICourseDetailsCardProps{
+	data: IDetailedCourse | undefined;
+};
+
+export function CourseDetailsCard({data}: ICourseDetailsCardProps): ReactElement {
+
+	const studentCount: number | undefined = data?.users.length;
+	const students: IUser[] | undefined = data?.users;
+	const modules: IModule[] | undefined = data?.modules;
+
+	if(data == null){
+		return <div className="g-card"></div>;
+	}
+
 	return (
 		<div className="g-card">
 			<h2 className="g-card-header">Course Details</h2>
-			<h3 className="g-list-item-header">Introduction to React</h3>
-			<p className="n-students g-list-item-text">Number of students: 4</p>
+			<h3 className="g-list-item-header">{data?.courseName}</h3>
+			<p className="n-students g-list-item-text">Number of students: {studentCount}</p>
 			<CourseDetailsStudentsDropdown />
 			<h3 className="g-list-item-header">Modules</h3>
 			<CourseDetailsModuleDropdown />
--- src/components/CourseItem.tsx
+++ src/components/CourseItem.tsx
@@ -9,12 +9,12 @@ interface ICourseItem {
 
 export default function CourseItem({ data, toggleActiveCourse, isActive }: ICourseItem): ReactElement {
 	return (
-		<li className={`g-list-item g-border-style ${isActive ? "g-active" : ""}`} onClick={() => toggleActiveCourse(data.id)}>
-			<h3 className="g-list-item-header">{data.name}</h3>
+		<li className={`g-list-item g-border-style ${isActive ? "g-active" : ""}`} onClick={() => toggleActiveCourse(data.courseId)}>
+			<h3 className="g-list-item-header">{data.courseName}</h3>
 			<div className="g-list-item-text">
-				{data.startdate} - {data.startdate}
+				{data.startDate} - {data.startDate}
 			</div>
-			<div className="g-list-item-text">Teacher: {data.primary_teacher}</div>
+			<div className="g-list-item-text">Teacher: {data.teacher}</div>
 		</li>
 	);
 }
