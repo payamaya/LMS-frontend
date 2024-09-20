2ed4cac7b63d420f1892ff8115d11124c0de2c61
vikkoooo <vikkolundberg@gmail.com>
2024-09-20 15:00:38 +0200

    clear temporary interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,56 +1,11 @@
-export interface BasicCourseInfo {
-	id: number,
-	name: string,
-	primary_teacher: string,
-	startdate: string
-}
-
-// interfaces.ts
-export interface User {
+export interface IMockData {
 	userId: number;
 	name: string;
 	email: string;
+	course: IDetailedCourse;
 }
 
-export interface Activity {
-	activityId: number;
-	activityName: string;
-	description: string;
-	startTime: string; // ISO date string
-	endTime: string;   // ISO date string
-	activityType: {
-		activityTypeId: number;
-		activityTypeName: string;
-	};
-}
-
-export interface Module {
-	moduleId: number;
-	moduleName: string;
-	description: string;
-	startDate: string;
-	endDate: string;
-	state: string;
-	activities: Activity[];
-}
-
-export interface Course {
-	courseId: number;
-	courseName: string;
-	description: string;
-	startDate: string;
-	users: User[];
-	modules: Module[];
-}
-
-export interface MockData {
-	userId: number;
-	name: string;
-	email: string;
-	course: Course;
-}
-
-export interface MockContext {
-	data: MockData;
+export interface IMockContext {
+	data: IMockData;
 	isLoading: boolean;
 }
