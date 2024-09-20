c59400c0be7f9ac08af9931316854822848d1d75
OskarMartens <martens.oskar@gmail.com>
2024-09-20 14:59:10 +0200

    Update interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,50 @@
+export interface IActivity {
+	activityId: number;
+	activityName: string;
+	description: string;
+	startTime: string;
+	endTime: string;
+	activityType: IActivityType;
+}
+
+export interface IActivityType {
+	activityTypeId: number;
+	activityTypeName: string;
+}
+
+export interface IBasicCourseInfo {
+	courseId: number,
+	courseName: string,
+	description: string,
+	startDate: string
+	teacher: string,
+}
+
+export interface IDetailedCourse {
+	courseId: number;
+	courseName: string;
+	description: string;
+	startDate: string;
+	users: IUser[];
+	modules: IModule[];
+}
+
+export interface IModule {
+	moduleId: number;
+	moduleName: string;
+	description: string;
+	startDate: string;
+	endDate: string;
+	state: string;
+	activities: IActivity[];
+}
+
+export interface IUser {
+	userId: number;
+	name: string;
+	email: string;
+}
+
 export interface IMockData {
 	userId: number;
 	name: string;
