6b2a6be6c394c1a4e1f2efb43b54c1b83809cc0c
OskarMartens <martens.oskar@gmail.com>
2024-09-20 14:59:24 +0200

    TeacherDashboard using dynamic values.

--- src/pages/TeacherDashboardPage.tsx
+++ src/pages/TeacherDashboardPage.tsx
@@ -1,44 +1,42 @@
-import { ReactElement, useState } from "react";
-import { BasicCourseInfo } from "../interfaces";
+import { ReactElement, useEffect, useState } from "react";
+import { IBasicCourseInfo, IDetailedCourse } from "../interfaces";
 import { CourseDetailsCard } from "../components/CourseDetailsCard";
+import data from "../courses-mock.json";
+import detailedData from "../courses-courseid-mock.json";
 import CourseItem from "../components/CourseItem";
 
 export function TeacherDashboardPage(): ReactElement {
-    const data: BasicCourseInfo[] =
-	[
-        {
-            id: 2,
-            name: "Introduction to c#",
-            primary_teacher: "Linus Torvalds",
-            startdate: "2024-05-17",
-        },
-    ];
+	const [activeCourse, setActiveCourse] = useState<IBasicCourseInfo | null>(null);
+	const [detailedCourse, setDetailedCourse] = useState<IDetailedCourse>();
 
-    const [activeCourse, setActiveCourse] = useState<BasicCourseInfo>(data[0]);
+	useEffect(() => {
+		setDetailedCourse(detailedData.find((detailedCourse) => detailedCourse.courseId == activeCourse?.courseId));
+	}, [activeCourse]);
 
-    const toggleActiveCourse = (id: number): void => {
-        const selectedCourse = data.find((course) => course.id === id);
-        if (selectedCourse) setActiveCourse(selectedCourse);
-    };
+	const toggleActiveCourse = (id: number): void => {
+		const selectedCourse = data.find((course) => course.courseId === id);
+		if (selectedCourse) setActiveCourse(selectedCourse);
+	};
 
-    return (
-        <div className="teacher-dashboard-container g-container">
-            <h1 className="g-page-header">Teacher Dashboard</h1>
-            <div className="g-page-card-container">
-                <div className="g-card">
-                    <h2 className="g-card-header">Courses</h2>
-                    <ul className="g-list">
-                        {data.map((item) => (
-                            <CourseItem
-								key={item.id}
+	return (
+		<div className="teacher-dashboard-container g-container">
+			<h1 className="g-page-header">Teacher Dashboard</h1>
+			<div className="g-page-card-container">
+				<div className="g-card">
+					<h2 className="g-card-header">Courses</h2>
+					<ul className="g-list">
+						{data.map((item) => (
+							<CourseItem
+								key={item.courseId}
 								data={item}
 								toggleActiveCourse={toggleActiveCourse}
-								isActive={activeCourse.id === item.id} />
-                        ))}
-                    </ul>
-                </div>
-                <CourseDetailsCard />
-            </div>
-        </div>
-    );
+								isActive={activeCourse?.courseId === item.courseId}
+							/>
+						))}
+					</ul>
+				</div>
+				<CourseDetailsCard data={detailedCourse} />
+			</div>
+		</div>
+	);
 }
