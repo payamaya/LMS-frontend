d26708390f402d0800b75c8e51c6d84bb671f958
OskarMartens <martens.oskar@gmail.com>
2024-09-26 15:30:14 +0200

    Fetch student data on redirection to student page

--- src/components/StudentCourseCard.tsx
+++ src/components/StudentCourseCard.tsx
@@ -1,10 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import { IContext } from "../utils/interfaces";
 import { CourseDetailsStudentsDropdown } from "../components/CourseDetailsStudentsDropdown";
 
 export function StudentCourseCard(): ReactElement {
-	const { studentMockData } = useOutletContext<IContext>();
+	// const { studentMockData } = useOutletContext<IContext>();
+	const {fetchCourseForStudent, detailedCourse} = useOutletContext<IContext>();
 
 	// helper function: get state text from state
 	const getModuleStateLabel = (state: string): string => {
@@ -20,22 +21,27 @@ export function StudentCourseCard(): ReactElement {
 		}
 	};
 
+	useEffect(() => {
+		fetchCourseForStudent();
+	},[])
+
 	return (
-		<div className="g-card">
-			<h2 className="g-card-header">{studentMockData.course.courseName}</h2>
-			<p className="g-text-subheading">Your current course</p>
-			<h3 className="g-list-item-header">Participants</h3>
-			<p className="n-students g-list-item-text">{studentMockData.course.users.length} students enrolled</p>
-			<CourseDetailsStudentsDropdown students={studentMockData.course.users} />
-			<h3 className="g-list-item-header">Modules</h3>
-			<ul className="g-list">
-				{studentMockData.course.modules.map((module) => (
-					<li key={module.moduleId} className="course-modules-list-item">
-						<span className="g-text">{module.moduleName}</span>
-						<span className={`g-text ${module.state}`}>{getModuleStateLabel(module.state)}</span>
-					</li>
-				))}
-			</ul>
-		</div>
+		// <div className="g-card">
+		// 	<h2 className="g-card-header">{studentData?.course.courseName}</h2>
+		// 	<p className="g-text-subheading">Your current course</p>
+		// 	<h3 className="g-list-item-header">Participants</h3>
+		// 	<p className="n-students g-list-item-text">{studentData?.course.users.length} students enrolled</p>
+		// 	<CourseDetailsStudentsDropdown students={studentData?.course.users} />
+		// 	<h3 className="g-list-item-header">Modules</h3>
+		// 	<ul className="g-list">
+		// 		{studentData?.course.modules.map((module) => (
+		// 			<li key={module.moduleId} className="course-modules-list-item">
+		// 				<span className="g-text">{module.moduleName}</span>
+		// 				<span className={`g-text ${module.state}`}>{getModuleStateLabel(module.state)}</span>
+		// 			</li>
+		// 		))}
+		// 	</ul>
+		// </div>
+		<div>Hej</div>
 	);
 }
