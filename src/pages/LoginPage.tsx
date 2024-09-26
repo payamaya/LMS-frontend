22f812e5be0ac57ad1d60cec6f79f6ba76eb1b69
OskarMartens <martens.oskar@gmail.com>
2024-09-26 12:22:04 +0200

    Add routing depending on role.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,48 +1,37 @@
-import { FormEventHandler, ReactElement, useContext, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { FormEventHandler, ReactElement, useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authProvider";
 import { jwtDecode } from "jwt-decode";
 
 export function LoginPage(): ReactElement {
 	const [username, setUsername] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
-	const { isLoggedIn, login} = useContext(AuthContext);
+	const { isLoggedIn, login } = useContext(AuthContext);
 	const navigate = useNavigate();
 
-	if (isLoggedIn) {
-		const token = localStorage.getItem('tokens');
-		if (token) {
-			const decodedToken = jwtDecode(token);
-			console.log(decodedToken);
-			console.log(decodedToken.aud);
-			console.log(decodedToken.exp);
-			console.log(decodedToken.iat?.toString());
-			console.log(decodedToken.iss);
-			console.log(decodedToken.jti);
-			console.log(decodedToken.nbf);
-			console.log(decodedToken.sub);
-			// const userRole = decodedToken.;
-	   
-	   
-		// 	if (userRole === 'Admin') {
-		// 	  navigate('/admin-dashboard');
-		// 	} else if (userRole === 'User') {
-		// 	  navigate('/user-dashboard');
-		// 	} else {
-		// 	  navigate('/login');
-		// 	}
-		//   } else {
-		// 	navigate('/login'); 
+	useEffect(() => {
+		if (isLoggedIn) {
+			const token = localStorage.getItem("tokens");
+			if (token) {
+				const decodedToken: any = jwtDecode(token);
+				const userRole: string = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+				console.log(userRole);
 
-		  }
-		return <Navigate to="/teacher" replace />;
-	}
+				if (userRole === "Teacher") {
+					navigate("/teacher");
+				} else if (userRole === "Student") {
+					navigate("/student");
+				}
+			} else {
+				navigate("/");
+			}
+		}
+	}, [isLoggedIn, navigate]);
 
 	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
 		e.preventDefault();
 
 		await login(username, password);
-		navigate("/student");
 	};
 
 	return (
