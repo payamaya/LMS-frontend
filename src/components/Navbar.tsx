b82206557781c381221f047899a462f32ce0a12b
vikkoooo <vikkolundberg@gmail.com>
2024-09-30 10:20:45 +0200

    conditional rendering of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authProvider";
+import { jwtDecode } from "jwt-decode";
 
 export function Navbar(): ReactElement {
-	const {logout} = useContext(AuthContext);
+	const { logout } = useContext(AuthContext);
+	const [role, setRole] = useState<string>();
+
+	const tempDecodeTokenRole = () => {
+		const token: any = localStorage.getItem("tokens");
+		const decodedToken: any = jwtDecode(token);
+		const userRole: string = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+		setRole(userRole);
+	}
+
+	useEffect(() => {
+		tempDecodeTokenRole();
+	}, []);
 
 	return (
 		<header className="navbar">
@@ -11,9 +24,15 @@ export function Navbar(): ReactElement {
 				<span className="material-symbols-outlined navbar-avatar">person</span>
 			</Link>
 			<nav className="navbar-links">
-				<Link to="/student" className="navbar-link-student-dashboard">Student Dashboard</Link>
-				<Link to="/teacher" className="navbar-link-teacher-dashboard">Teacher Dashboard</Link>
-				<Link to="/course/1" className="navbar-link-course-details">Course Details 1</Link>
+				{role === "Student" && (
+					<Link to="/student" >Student Dashboard</Link>
+				)}
+				{role === "Teacher" && (
+					<>
+						<Link to="/teacher" >Teacher Dashboard</Link>
+						<Link to="/course/1" >Course Details</Link>
+					</>
+				)}
 			</nav>
 			<button type="button" onClick={logout} className="navbar-button-logout">Logout</button>
 		</header>
