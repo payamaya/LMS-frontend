1e0e58383304c7f82411d0267de60d0816977117
payamaya <paulyashou81@gmail.com>
2024-09-30 15:47:56 +0200

    Remove link Button according to role

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,40 @@
-import { ReactElement, useContext } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../context/authProvider";
+import { ReactElement, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/authProvider'
 
 export function Navbar(): ReactElement {
-	const {logout} = useContext(AuthContext);
+  const { isLoggedIn, userRole, logout } = useContext(AuthContext) || {
+    isLoggedIn: false,
+    userRole: null,
+  }
 
-	return (
-		<header className="navbar">
-			<Link to="/" className="navbar-link-home">
-				<span className="material-symbols-outlined navbar-avatar">person</span>
-			</Link>
-			<nav className="navbar-links">
-				<Link to="/student" className="navbar-link-student-dashboard">Student Dashboard</Link>
-				<Link to="/teacher" className="navbar-link-teacher-dashboard">Teacher Dashboard</Link>
-				<Link to="/course/1" className="navbar-link-course-details">Course Details 1</Link>
-			</nav>
-			<button type="button" onClick={logout} className="navbar-button-logout">Logout</button>
-		</header>
-	);
+  return (
+    <header className='navbar'>
+      <Link to='/' className='navbar-link-home'>
+        <span className='material-symbols-outlined navbar-avatar'>person</span>
+      </Link>
+      <nav className='navbar-links'>
+        {isLoggedIn && userRole === 'Student' && (
+          <Link to='/student' className='navbar-link-student-dashboard'>
+            Student Dashboard
+          </Link>
+        )}
+        {isLoggedIn && userRole === 'Teacher' && (
+          <Link to='/teacher' className='navbar-link-teacher-dashboard'>
+            Teacher Dashboard
+          </Link>
+        )}
+        {isLoggedIn && userRole === 'Teacher' && (
+          <Link to='/course/1' className='navbar-link-course-details'>
+            Course Details
+          </Link>
+        )}
+      </nav>
+      {isLoggedIn && (
+        <button type='button' onClick={logout} className='navbar-button-logout'>
+          Logout
+        </button>
+      )}
+    </header>
+  )
 }
