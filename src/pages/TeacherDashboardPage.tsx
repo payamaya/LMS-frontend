2363adcd6fc4ced9fbff2857a03b96527d8d92e7
payamaya <paulyashou81@gmail.com>
2024-10-03 09:18:14 +0200

    Add ModalOverlay to TeacherDashboard

--- src/pages/TeacherDashboardPage.tsx
+++ src/pages/TeacherDashboardPage.tsx
@@ -1,15 +1,44 @@
-import { ReactElement } from "react";
-import { CourseDetailsCard } from "../components/CourseDetailsCard";
-import CourseListCard from "../components/CourseListCard";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactElement, useState } from 'react'
+import { CourseDetailsCard } from '../components/CourseDetailsCard'
+import CourseListCard from '../components/CourseListCard'
+import { ReusableBtn } from '../components/ReusableBtn'
+import { ModalOverlay } from '../components/ModalOverlay'
 
 export function TeacherDashboardPage(): ReactElement {
-	return (
-		<div className="teacher-dashboard-container g-container">
-			<h1 className="g-page-header">Teacher Dashboard</h1>
-			<div className="g-page-card-container">
-				<CourseListCard />
-				<CourseDetailsCard />
-			</div>
-		</div>
-	);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleAddClick = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleSubmit = (formData: any) => {
+    console.log('Form Data Submitted:', formData)
+    // Add logic to handle the form data submission
+  }
+
+  return (
+    <div className='teacher-dashboard-container g-container'>
+      <section className='teacher-dashboard-btn'>
+        <h1 className='g-page-header'>Teacher Dashboard</h1>
+        <ReusableBtn className='btn-submit' onClick={handleAddClick}>
+          Add
+        </ReusableBtn>
+      </section>
+      <div className='g-page-card-container'>
+        <CourseListCard />
+        <CourseDetailsCard />
+      </div>
+
+      <ModalOverlay
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSubmit={handleSubmit} // Pass handleSubmit as a prop
+      />
+    </div>
+  )
 }
