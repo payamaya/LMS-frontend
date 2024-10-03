aba7dd31b9648dd137725733cb45d4269947e14e
payamaya <paulyashou81@gmail.com>
2024-10-03 18:03:02 +0200

    add DeleteCourse component

--- src/components/CourseItem.tsx
+++ src/components/CourseItem.tsx
@@ -1,20 +1,42 @@
-import { ReactElement } from "react";
-import { IBasicCourseInfo } from "../utils/interfaces";
+import { ReactElement } from 'react'
+import { IBasicCourseInfo } from '../utils/interfaces'
+import DeleteCourse from './DeleteCourse'
 
 interface ICourseItem {
-	data: IBasicCourseInfo;
-	toggleActiveCourse: (id: string) => void;
-	isActive: boolean;
+  data: IBasicCourseInfo
+  toggleActiveCourse: (id: string) => void
+  isActive: boolean
+  onDeleteSuccess: (deletedCourseId: string) => void // New prop for handling course deletion
 }
 
-export default function CourseItem({ data, toggleActiveCourse, isActive }: ICourseItem): ReactElement {
-	return (
-		<li className={`g-list-item g-border-style ${isActive ? "g-active" : ""}`} onClick={() => toggleActiveCourse(data.id)}>
-			<h3 className="g-list-item-header">{data.courseName}</h3>
-			<div className="g-list-item-text">
-				{new Date(data.startDate).toLocaleDateString()} - {new Date(data.startDate).toLocaleDateString()}
-			</div>
-			<div className="g-list-item-text">Teacher: {data.teacher?.name}</div>
-		</li>
-	);
+export default function CourseItem({
+  data,
+  toggleActiveCourse,
+  isActive,
+  onDeleteSuccess,
+}: ICourseItem): ReactElement {
+  const handleDeleteSuccess = () => {
+    onDeleteSuccess(data.id)
+  }
+
+  return (
+    <li
+      className={`g-list-item g-border-style ${isActive ? 'g-active' : ''}`}
+      onClick={() => toggleActiveCourse(data.id)}
+    >
+      <h3 className='g-list-item-header'>{data.courseName}</h3>
+      <div className='g-list-item-text'>
+        {new Date(data.startDate).toLocaleDateString()} -{' '}
+        {new Date(data.startDate).toLocaleDateString()}
+      </div>
+      <div className='g-list-item-text'>Teacher: {data.teacher?.name}</div>
+
+      {/* Pass handleDeleteSuccess to DeleteCourse */}
+      <DeleteCourse
+        resourceId={data.id}
+        onSuccess={handleDeleteSuccess}
+        resourceType='courses'
+      />
+    </li>
+  )
 }
